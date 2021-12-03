af919e644d0e4dfb71c9423316f261df47667f55
Robert Evanik <rob.evanik@gmail.com>
2021-12-02 21:59:10 -0500

    accessing team and opponent scores per quarter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var team = "NYJ"
-var week = 12;
+var week = 1;
+
 var AFCNum = 5;
 var NFCNum = 5;
 var qt1Payout = 100;
@@ -7,29 +8,50 @@ var qt2Payout = 150;
 var qt3Payout = 100;
 var qt4Payout = 300;
 
+var homeScores = {
+    q1: null,
+    q2: null,
+    q3: null,
+    q4: null,
+    overtime: null,
+}
+
+var awayScores = {
+    q1: null,
+    q2: null,
+    q3: null,
+    q4: null,
+    overtime: null,
+}
+
 var teamsArray = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC", "LAC",
-    "LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS",];
+    "LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS"];
 
 
 
-var getScore = function(team) {
+
+
+var getTeamData = function(team) {
     var key = "3e0e0d8d140747b997880c8e9c121ac8";
     var season = "2021";
     let apiUrl = `https://api.sportsdata.io/v3/nfl/scores/json/TeamGameStats/${season}/${week}?key=${key}`
 
+    //localStorage.clear();
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-
                     for (var i = 0; i < data.length; i++) {
                         localStorage.setItem("ID" + i, JSON.stringify(data[i]));
                         if (data[i].Team === team) {
+                            console.log("Selected Team " + data[i].Team);
                             console.log(data[i]);
-                            getOpponent(i, team);
+                            getOpponentData(i, data[i].GameKey);
+                            getSelectedTeamScore(data[i])
                         }
                     }
-
+                    console.log(awayScores)
+                    console.log(homeScores)
                 });
             }
             else {
@@ -38,22 +60,46 @@ var getScore = function(team) {
         })
 }
 
-var getOpponent = function(index, team) {
+var getOpponentData = function(index, gameKey) {
     data = JSON.parse(localStorage.getItem("ID" + index));
     var opp = data.Opponent
-    console.log(opp);
 
     // Finds Team Data for the opponent in local storage
-    for (var i = 0; i < 30; i++) {
+    for (var i = 0; i < localStorage.length; i++) {
         parsedData = JSON.parse(localStorage.getItem("ID" + i));
-        if (parsedData.Team === opp) {
+        // Check for the oppent team and select the matching game by gamekey to avoid dupilicates if a team plays another team more than once
+        if (parsedData.Team === opp && data.GameKey === gameKey) {
+            console.log("Opponent " + opp);
             console.log(parsedData);
+            if (parsedData.HomeOrAway === "HOME") {
+                homeScores.q1 = parsedData.ScoreQuarter1;
+                homeScores.q2 = parsedData.ScoreQuarter2;
+                homeScores.q3 = parsedData.ScoreQuarter3;
+                homeScores.q4 = parsedData.ScoreQuarter4;
+            } else {
+                awayScores.q1 = parsedData.ScoreQuarter1;
+                awayScores.q2 = parsedData.ScoreQuarter2;
+                awayScores.q3 = parsedData.ScoreQuarter3;
+                awayScores.q4 = parsedData.ScoreQuarter4;
+            }
         }
     }
 }
 
+var getSelectedTeamScore = function(data) {
 
-
+    if (data.HomeOrAway === "HOME") {
+        homeScores.q1 = parsedData.ScoreQuarter1;
+        homeScores.q2 = parsedData.ScoreQuarter2;
+        homeScores.q3 = parsedData.ScoreQuarter3;
+        homeScores.q4 = parsedData.ScoreQuarter4;
+    } else {
+        awayScores.q1 = parsedData.ScoreQuarter1;
+        awayScores.q2 = parsedData.ScoreQuarter2;
+        awayScores.q3 = parsedData.ScoreQuarter3;
+        awayScores.q4 = parsedData.ScoreQuarter4;
+    }
+}
 
 
 
@@ -87,5 +133,5 @@ var getGif = function() {
         })
 }
 
-getScore(team);
+getTeamData(team);
 //getGif();
