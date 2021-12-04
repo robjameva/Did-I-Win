1e16f06bb3d8dcda848a3b0497fa394337adde38
Robert Evanik <rob.evanik@gmail.com>
2021-12-04 16:55:00 -0500

    Added comments to code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// HTML selectors
 var gameWeekDropDownEl = document.getElementById("week-btn-list")
 var gameWeekTitleEL = document.getElementById("week-dropdown")
 var gameWeekEl = document.querySelector(".week-btn")
@@ -14,7 +15,7 @@ var awayScoreDropDownEl = document.getElementById("away-score-btn-list")
 var awayScoreTitleEL = document.getElementById("away-score-dropdown")
 var awayScoreEl = document.querySelector(".away-score-btn")
 
-
+// Global Variables
 var team = null;
 var week = null;
 
@@ -43,18 +44,17 @@ var awayScores = {
     overtime: null
 }
 
-var teamsArray = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC", "LAC",
-    "LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS"];
+//var teamsArray = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC", "LAC","LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS"];
 
-
-
-
-
+// Main API Call to Sports Data IO
 var getTeamData = function(team) {
     var key = "3e0e0d8d140747b997880c8e9c121ac8";
     var season = "2021";
     let apiUrl = `https://api.sportsdata.io/v3/nfl/scores/json/TeamGameStats/${season}/${week}?key=${key}`
 
+    // In this API call we get the data for every game for the week that is specified in the URL
+    // Each game data object is saved to session storage
+    // We loop through each object and when we find the object matching the team that we selected then we save that Game Key to local storage
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
@@ -81,7 +81,7 @@ var getTeamData = function(team) {
 
 
 
-// Sets local storage to save only the game objects for the selected team and their opponent
+// Sets local storage to save only the game objects for the selected team and their opponent by looking for the Game Key saved in the inital API call
 var setLocalStorage = function() {
     var gameKey = localStorage.getItem("GameKey");
     for (var i = 0; i < sessionStorage.length; i++) {
@@ -93,6 +93,7 @@ var setLocalStorage = function() {
     }
 }
 
+// Sets the global obejcts for home and away teams with the relevent data from local storage
 var setTeamData = function() {
     var homeTeamData = localStorage.getItem("HOME");
     var awayTeamData = localStorage.getItem("AWAY");
@@ -112,37 +113,7 @@ var setTeamData = function() {
     awayScores.q4 = awayTeamParsed.ScoreQuarter4;
 }
 
-var didIWin = function() {
-    // Get total score per quarter and only look at the last didgit
-    var homeFirstQuarter = homeScores.q1 % 10;
-    var homeSecondQuarter = (homeScores.q1 + homeScores.q2) % 10;
-    var homeThirdQuarter = (homeScores.q1 + homeScores.q2 + homeScores.q3) % 10;
-    var homeFourthQuarter = (homeScores.q1 + homeScores.q2 + homeScores.q3 + homeScores.q4) % 10;
-
-    var awayFirstQuarter = awayScores.q1 % 10;
-    var awaySecondQuarter = (awayScores.q1 + awayScores.q2) % 10;
-    var awayThirdQuarter = (awayScores.q1 + awayScores.q2 + awayScores.q3) % 10;
-    var awayFourthQuarter = (awayScores.q1 + awayScores.q2 + awayScores.q3 + awayScores.q4) % 10;
-
-    console.log(homeFirstQuarter, homeSecondQuarter, homeThirdQuarter, homeFourthQuarter)
-    console.log(awayFirstQuarter, awaySecondQuarter, awayThirdQuarter, awayFourthQuarter)
-
-    if (homeNum === homeFirstQuarter && awayNum === awayFirstQuarter) {
-        console.log("You Won $" + qt1Payout)
-    }
-
-    if (homeNum === homeSecondQuarter && awayNum === awaySecondQuarter) {
-        console.log("You Won $" + qt2Payout)
-    }
-    if (homeNum === homeThirdQuarter && awayNum === awayThirdQuarter) {
-        console.log("You Won $" + qt3Payout)
-    }
-    if (homeNum === homeFourthQuarter && awayNum === awayFourthQuarter) {
-        console.log("You Won $" + qt4Payout)
-    }
-
-}
-
+// Dynamically creates score buttons for the dropdown to include the team name
 var createScoreBtns = function(num, homeOrAway, team) {
     var scoreLi = document.createElement("li");
     var scoreBtn = document.createElement("button");
@@ -160,6 +131,7 @@ var createScoreBtns = function(num, homeOrAway, team) {
     }
 }
 
+// Checks if dropdown btns exisit and clears them
 var checkDropdownChildren = function() {
     if (homeScoreDropDownEl.hasChildNodes && awayScoreDropDownEl.hasChildNodes) {
         homeScoreDropDownEl.innerHTML = "";
@@ -167,6 +139,7 @@ var checkDropdownChildren = function() {
     }
 }
 
+// Function to call createScoreBtns for both the home and away teams
 var renderScoreBtns = function() {
     for (var i = 0; i < 10; i++) {
         createScoreBtns(i, "home", homeScores.team);
@@ -174,10 +147,38 @@ var renderScoreBtns = function() {
     }
 }
 
+// Logic to check if the users numbers match the winning numbers
+var didIWin = function() {
+    // Get total score per quarter and only look at the last didgit
+    var homeFirstQuarter = homeScores.q1 % 10;
+    var homeSecondQuarter = (homeScores.q1 + homeScores.q2) % 10;
+    var homeThirdQuarter = (homeScores.q1 + homeScores.q2 + homeScores.q3) % 10;
+    var homeFourthQuarter = (homeScores.q1 + homeScores.q2 + homeScores.q3 + homeScores.q4) % 10;
 
+    var awayFirstQuarter = awayScores.q1 % 10;
+    var awaySecondQuarter = (awayScores.q1 + awayScores.q2) % 10;
+    var awayThirdQuarter = (awayScores.q1 + awayScores.q2 + awayScores.q3) % 10;
+    var awayFourthQuarter = (awayScores.q1 + awayScores.q2 + awayScores.q3 + awayScores.q4) % 10;
 
+    console.log(homeFirstQuarter, homeSecondQuarter, homeThirdQuarter, homeFourthQuarter)
+    console.log(awayFirstQuarter, awaySecondQuarter, awayThirdQuarter, awayFourthQuarter)
 
+    // Since the same numbers can win multiple quarters we check each quarter against our numbers with individual if statements
+    if (homeNum === homeFirstQuarter && awayNum === awayFirstQuarter) {
+        console.log("You Won $" + qt1Payout)
+    }
 
+    if (homeNum === homeSecondQuarter && awayNum === awaySecondQuarter) {
+        console.log("You Won $" + qt2Payout)
+    }
+    if (homeNum === homeThirdQuarter && awayNum === awayThirdQuarter) {
+        console.log("You Won $" + qt3Payout)
+    }
+    if (homeNum === homeFourthQuarter && awayNum === awayFourthQuarter) {
+        console.log("You Won $" + qt4Payout)
+    }
+
+}
 
 
 
@@ -207,7 +208,7 @@ var getGif = function() {
 //getGif();
 
 
-
+// Event Listeners
 gameWeekDropDownEl.addEventListener("click", function(event) {
     var btn = event.target;
     gameWeekTitleEL.textContent = btn.textContent;
